42827d87f4812fa9133161cefee62fb03a252236
tasawwuramd <tasawwuramd@gmail.com>
2020-09-23 23:55:41 +0530

    started with whatsapp clone

---
+++ whatsapp-mern/src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD1I52r165xx6v0dCOMbK7y8hxID9w_P50",
+    authDomain: "whatsapp-mern-9c92f.firebaseapp.com",
+    databaseURL: "https://whatsapp-mern-9c92f.firebaseio.com",
+    projectId: "whatsapp-mern-9c92f",
+    storageBucket: "whatsapp-mern-9c92f.appspot.com",
+    messagingSenderId: "308614211944",
+    appId: "1:308614211944:web:8adf990948799f9f378edd"
+  };
